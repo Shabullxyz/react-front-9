ddaef8b6a11c588a7600124d663dd9cc8e8277ad
Shabull <shabull1997@gmail.com>
2024-05-16 18:23:31 -0300

    Add Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import {AddStock} from "./pages/AddStock"
 import {SellStock } from "./pages/SellStock"
 import {Login} from "./pages/Login"
 import {Register} from "./pages/Register"
+import {Navigation } from './components/Navigation'
+import { Home } from './pages/Home'
 
 function App() {
   return(
     <BrowserRouter>
-    <Routes>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/register" element={<Register/>}/>
-      <Route path="/stock" element={<Stock/>}/>
-      <Route path="/AddStock" element={<AddStock/>}/>
-      <Route path="/SellStock" element={<SellStock/>}/>
-    </Routes>
+      <Navigation/>
+      <Routes>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/stock" element={<Stock/>}/>
+        <Route path="/AddStock" element={<AddStock/>}/>
+        <Route path="/SellStock" element={<SellStock/>}/>
+      </Routes>
     </BrowserRouter>
 
     
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+import { Home } from "../pages/Home"
+
+export function Navigation() {
+  return (
+    <div>
+        <Link to="/Home">Logo</Link>
+    </div>
+  )
+}
+
