b402486081e8c54d5903e038a880e887831c1f8a
Shabull <shabull1997@gmail.com>
2024-05-20 16:06:20 -0300

    Antes de empezar a hacer peticiones

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
     <BrowserRouter>
       <Navigation/>
       <Routes>
+        <Route path="/" element={<Home/>} />
         <Route path="/home" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,7 +4,7 @@ import { Home } from "../pages/Home"
 export function Navigation() {
   return (
     <div>
-        <Link to="/Home">Logo</Link>
+        <Link to="/">Logo</Link>
     </div>
   )
 }
